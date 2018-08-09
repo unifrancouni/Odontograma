55052e199fa85b11ac08b11e07503eb35ebc1be3
Franco Antonio Ugarte Calderon <franco.ugarte@usuracero.gob.ni>
2018-08-09 15:49:30 -0600

    Migrating to MVC C#

---
+++ SIGECO/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIGECO.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Simbolos(int id)
+        {
+            var db = new SIGECO_Entities();
+            var consulta = db.vwSimboloDiente
+                .Where(c => c.nExpedienteID == id)
+                .Select(c => new { c.sNombreDiente, c.sDescripcion }).ToList();
+            return Json(consulta, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
